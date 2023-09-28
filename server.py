d233d6d7e4dd4ddabcae6759fc7d7cbac3ff1183
Tianxiang Ren <tianxia3@ualberta.ca>
2023-09-28 13:28:35 -0600

    feat: implement server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #  coding: utf-8 
 import socketserver
+from typing import Optional
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -27,13 +29,108 @@ import socketserver
 # try: curl -v -X GET http://127.0.0.1:8080/
 
 
+def parseHttpRequest(request: str) -> Optional[dict]:
+    try:
+        supported_header = []
+        parsed_value = {}
+        tokens = request.split()
+        parsed_value['method'] = tokens[0]
+        parsed_value['pathname'] = tokens[1]
+        tokens = request.split("\r\n")
+        
+        return parsed_value
+    except:
+        return None;
+
+def createHtmlResponse(content: str) -> bytes:
+    response = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r' + content + '\r\n'
+    return response.encode()
+
+def createCssResponse(content: str) -> bytes:
+    response = 'HTTP/1.1 200 OK\r\nContent-Type: text/css; charset=UTF-8\r\n\r' + content + '\r\n'
+    return response.encode()
+
+def createPlainTextResponse(content: str) -> bytes:
+    response = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\n\r' + content + '\r\n'
+    return response.encode()
+
+
+def create301Response(location: str) -> bytes:
+    response = 'HTTP/1.1 301 Moved Permanentl\r\nLocation: ' + location +'\r\n'
+    return response.encode()
+
+def create404Response(message: str) -> bytes:
+    response = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/plain; charset=UTF-8\r\n\r\r' + message + '\r\n'
+    return response.encode()
+
+def isValidPath(path: str) -> bool:
+    abs_path = os.path.abspath(path)
+    accessiable_path = os.path.abspath('./www')
+    if abs_path.startswith(accessiable_path):
+        return True
+    return False
+
+def handle_GET(pathname) -> bytes:
+    try:
+        file_path = ""
+        file_type = ""
+        if pathname.endswith('/'):
+            file_path = './www' + pathname + 'index.html'
+        elif pathname.endswith('deep'):
+            response = create301Response('http://localhost:8080/deep/')
+            return response
+        else:
+            file_path = './www' + pathname
+        
+        if file_path.endswith(".html"):
+            file_type = "html"
+        elif file_path.endswith(".css"):
+            file_type = "css"
+        
+        if not isValidPath(file_path):
+            response = create404Response('')
+            return response
+
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+            response = ''
+            if file_type == 'html':
+                response = createHtmlResponse(file_content)
+            elif file_type == 'css':
+                response = createCssResponse(file_content)
+            else:
+                response = createPlainTextResponse(file_content)
+            
+            return response
+    except FileNotFoundError:
+        response = create404Response('')
+        return response
+                
+
+
+
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        unsupported_methods = set(['POST', 'PUT', 'HEAD', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'])
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        string_data = self.data.decode('utf-8')
+        parsed_request = parseHttpRequest(string_data)
+        http_method = parsed_request['method']
+        pathname = parsed_request['pathname']
 
+        print("Received:", string_data)
+
+        if http_method == 'GET':
+            response = handle_GET(pathname)
+            self.request.sendall(response)
+            print("Respond:", response.decode('utf-8'))
+        elif http_method in unsupported_methods:
+            response = b'HTTP/1.1 405 Method Not Allowed\r\n\r\nMethod Not Allowed!\r\n'
+            self.request.sendall(response)
+            print("Respond:", response.decode('utf-8'))
+
+        
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
